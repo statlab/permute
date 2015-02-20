2d9157bd6d09d081f6f13f60fc10556c9e08b241
Philip B. Stark <stark@risklaw.org>
2015-02-20 14:30:21 -0800

    add simulate_ts_dist

--- permute/irr.py
+++ permute/irr.py
@@ -47,3 +47,56 @@ def permute_rows(ratings):
     """
     np.apply_along_axis(np.random.shuffle, axis=1, arr=ratings)
     return True
+
+def simulate_ts_dist(ratings, obs_ts = None, iter=10000, keep_dist = False):
+    """
+    Simulates the permutation distribution of the irr test statistic for a matrix of
+    ratings <ratings>
+
+    If obs_ts is not null, computes the reference value of the test statistic before
+    the first permutation. Otherwise, uses the value obs_ts for comparison.
+
+    If <keep_dist>, return the distribution of values of the test statistic; otherwise,
+    return only the number of permutations for which the value of the irr test statistic is
+    at least as large as obs_ts.
+
+
+    Parameters
+    ----------
+    ratings : array_like
+              Input array of dimension [R, Ns]
+
+    obs_ts : float
+             observed value of the test statistic for the original data
+
+    iter : integer
+           number of random permutation of the elements of each row of ratings
+
+    keep_dist : bool
+                flag for whether to store and return the array of values of the irr
+                test statistic
+
+
+    Returns
+    -------
+    out : {obs_ts, geq, iter, dist}
+    obs_ts : observed value of the test statistic, or the input value of obs_ts if given
+    geq : number of iterations for which the test statistic was greater than or equal to
+          obs_ts
+    iter : iter
+    dist : if <keep_dist>, the array of values of the irr test statistic from the iter
+           iterations.  Otherwise, null.
+    """
+    if obs_ts is None:
+        obs_ts = compute_ts(ratings)
+    r = ratings.copy()
+    if keep_dist:
+        dist = np.zeros(iter)
+        dist = [compute_ts(permute_rows(r)) for i in range(iter)]
+        geq = np.sum(dist >= obs_ts)
+    else:
+        dist = None
+        geq = 0
+        for i in range(iter):
+            geq += compute_ts(permute_rows(r)) >= obs_ts
+    return {"obs_ts": obs_ts, "geq": geq, "iter": iter, "dist": dist}
