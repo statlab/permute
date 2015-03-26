2f6e3b4dec18779b81a8117528774bf0a8c2bcab
Jarrod Millman <jarrod.millman@gmail.com>
2015-03-26 16:30:27 -0700

    ENH: improve dependency handling

--- setup.py
+++ setup.py
@@ -9,11 +9,22 @@ DESCRIPTION = 'Permutation tests and confidence sets for Python'
 LONG_DESCRIPTION = descr
 MAINTAINER = 'Permute developers'
 MAINTAINER_EMAIL = 'permute@googlegroups.com'
-URL = 'http://github.com/permute/permute'
-LICENSE = 'BSD'
-DOWNLOAD_URL = 'http://github.com/permute/permute'
+URL = 'http://statlab.github.io/permute/'
+LICENSE = 'BSD License'
+DOWNLOAD_URL = 'http://github.com/statlab/permute'
 VERSION = '0.1dev'
 PYTHON_VERSION = (2, 7)
+
+INSTALL_REQUIRES = [
+    'numpy',
+    'scipy'
+]
+
+TESTS_REQUIRE = [
+    'coverage',
+    'nose',
+    'flake8'
+]
 
 from setuptools import setup
 
@@ -29,18 +40,23 @@ setup(
     download_url=DOWNLOAD_URL,
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Operating System :: MacOS',
     ],
+
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
 
     packages=['permute', 'permute.data', 'permute.tests'],
     package_data={'permute.data': ['*.csv', '*/*.csv', '*/*/*.csv']}
