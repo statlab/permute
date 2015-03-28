ade859b75be82577fc7dfe277fe1ce75be68cf21
Jarrod Millman <jarrod.millman@gmail.com>
2015-03-27 20:08:59 -0700

    DOC: move text around

--- permute/__init__.py
+++ permute/__init__.py
@@ -1,9 +1,59 @@
-"""Permutation tests and confidence intervals in Python
-
+"""
 ``permute`` provides permutation tests and confidence intervals for a variety
 of nonparametric testing and estimation problems, for a variety of
 randomization designs.
 
+* Stratified and unstratified tests ...
+* Test statistics in each stratum ...
+* Methods of combining tests across strata ...
+* Nonparametric combinations of tests ...
+
+Problems/Methods:
+-----------------
+
+1. The 2-sample problem
+2. The *n*-sample problem
+3. Tests for the slope in linear regression
+4. Tests for quantiles
+5. Tests of independence and association: runs tests, permutation association,  ...
+6. Tests of exchangeability
+7. Tests of symmetry: reflection, spherical, ...
+8. Permutation ANOVA
+9. Goodness of fit tests
+10. ...
+
+Confidence sets
+---------------
+
+1. Constant shifts
+2. Proportional shifts
+3. Monotone shifts
+4. ...
+
+Links
+-----
+
+UC Berkeley's Statistics 240: Nonparametric and Robust Methods.
+
+* `2015 course
+  website <http://www.stat.berkeley.edu/~johann/240spring15/index.html>`_
+* `Philip Stark's lecture
+  notes <http://www.stat.berkeley.edu/~stark/Teach/S240/Notes/index.htm>`_
+
+"Permutation Tests for Complex Data: Theory, Applications and Software"
+by Fortunato Pesarin, Luigi Salmaso
+
+
+* `Publisher's
+  website <http://www.wiley.com/WileyCDA/WileyTitle/productCd-0470516410.html>`_
+* `Supplementary Material (i.e., code and
+  data) <http://www.wiley.com/legacy/wileychi/pesarin/material.html>`_
+* `NPC test code <http://static.gest.unipd.it/~salmaso/NPC_TEST.htm>`_
+
+"Stochastic Ordering and ANOVA: Theory and Applications with R"
+by Basso D., Pesarin F., Salmaso L., Solari A.
+
+* `R code <http://static.gest.unipd.it/~salmaso/web/springerbook.htm>`_
 """
 
 import os.path as _osp
