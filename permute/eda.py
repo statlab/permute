5555cdcedb658cb59581329a96e58afd33ce94f5
Jarrod Millman <jarrod.millman@gmail.com>
2015-04-18 12:29:43 -0700

    ENH: helper function for data cleaning

---
+++ permute/eda.py
@@ -0,0 +1,39 @@
+""" Exploratory data analysis tools.
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+
+def find_duplicate_rows(x, as_string=False):
+    """ Find rows which are duplicated in x
+
+    Notes
+    -----
+    If you load a file, for example `nsgk.csv`, as a 2D array, say `x`,
+    then if you found '16,20,2,8' in the list returned by
+    ``find_duplicate_rows(x, as_string=True)`` you might do something like:
+
+        $ grep -n --context=1 '16,20,2,8' nsgk.csv
+        12512-16,15,2,8
+        12513:16,20,2,8
+        12514-16,45,2,8
+        --
+        12532-17,17,2,8
+        12533:16,20,2,8
+        12534-17,24,2,8
+
+
+    See
+    ---
+    http://stackoverflow.com/questions/8560440/removing-duplicate-columns-and-rows-from-a-numpy-2d-array
+    """
+    indx = np.lexsort(x.T)
+    x = x[indx]
+    diff = np.diff(x, axis=0)
+    indx = np.any(diff, axis=1)
+    dups = x[1:, :][~indx, ]
+    if as_string:
+        dups = [",".join([str(c) for c in r.tolist()]) for r in dups]
+    return dups
