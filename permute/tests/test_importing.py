205d39ef0733a6a8f3d6ad58baf32fbafda5727c
Jarrod Millman <jarrod.millman@gmail.com>
2015-04-26 16:51:17 -0700

    TST: try to check if missing nose dependency is handled

---
+++ permute/tests/test_importing.py
@@ -0,0 +1,17 @@
+from __future__ import division, print_function, absolute_import
+
+import sys
+
+import numpy as np
+from numpy.testing import (assert_equal,
+                           assert_almost_equal)
+
+from nose.tools import raises
+
+@raises(ImportError)
+def test_nose_import_error():
+    _tmp = sys.modules['nose']
+    sys.modules['nose'] = None
+    from permute import _test
+    _test()
+    sys.modules['nose'] = _tmp
