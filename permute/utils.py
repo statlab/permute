bacaad74c0caf61b8b5f520cef49e326bf0661a8
Jarrod Millman <jarrod.millman@gmail.com>
2015-05-02 12:22:13 -0700

    ENH: adapted check_random_state from sklearn

---
+++ permute/utils.py
@@ -0,0 +1,31 @@
+"""
+Various utilities and helper functions.
+"""
+
+import numbers
+
+import numpy as np
+
+def get_prng(seed=None):
+    """Turn seed into a np.random.RandomState instance
+
+    Parameters
+    ----------
+    seed : {None, int, RandomState}
+        If seed is None, return the RandomState singleton used by np.random.
+        If seed is an int, return a new RandomState instance seeded with seed.
+        If seed is already a RandomState instance, return it.
+        Otherwise raise ValueError.
+
+    Returns
+    -------
+    RandomState
+    """
+    if seed is None or seed is np.random:
+        return np.random.mtrand._rand
+    if isinstance(seed, (numbers.Integral, np.integer)):
+        return np.random.RandomState(seed)
+    if isinstance(seed, np.random.RandomState):
+        return seed
+    raise ValueError('%r cannot be used to seed a numpy.random.RandomState'
+                     ' instance' % seed)
