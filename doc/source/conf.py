e98370b49403b43956fda541ce355748232d4524
Stefan van der Walt <stefanv@berkeley.edu>
2015-08-14 15:13:36 -0700

    Write plots into build folder.  Add valid static directory.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -146,6 +146,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+ipython_savefig_dir = '../build/html/_static'
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
