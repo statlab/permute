3837a5a4a4e5d3e87ea5a5e3e2c42e466e40ca39
Kellie Ottoboni <kellieotto@berkeley.edu>
2015-09-25 16:50:00 -0700

    TST: Add tests to cover new lines, according to what coveralls said I missed

--- permute/tests/test_core.py
+++ permute/tests/test_core.py
@@ -106,6 +106,10 @@ def test_two_sample_shift():
     res = two_sample_shift(x, y, seed=42, shift=2)
     np.testing.assert_equal(res[0], 1)
     np.testing.assert_equal(res[1], expected_ts)
+    res2 = two_sample_shift(x, y, seed=42, shift=2, keep_dist=True)
+    np.testing.assert_equal(res2[0], 1)
+    np.testing.assert_equal(res2[1], expected_ts)
+    np.testing.assert_almost_equal(res2[2][:3], np.array([1.55886506,  0.87281296,  1.13611123]))
     res = two_sample_shift(x, y, seed=42, shift=2, alternative="less")
     np.testing.assert_equal(res[0], 0)
     np.testing.assert_equal(res[1], expected_ts)
@@ -122,7 +126,23 @@ def test_two_sample_shift():
     res = two_sample_shift(x, y, seed=42, shift=(f_err, f_err_inv), alternative="two-sided")
     np.testing.assert_equal(res[0], 0.17139)
     np.testing.assert_equal(res[1], expected_ts)
-    
+
+    # Define a lambda function
+    f = lambda u,v: np.max(u) - np.max(v)
+    res = two_sample(x, y, seed=42, stat=f, reps=100)
+    expected = (1, -3.2730653690015465)
+    np.testing.assert_equal(res[0], expected[0])
+    np.testing.assert_equal(res[1], expected[1])
+
+
+@raises(ValueError)
+def test_two_sample_bad_shift():
+    # Break it with a bad shift
+    x = np.array(range(5))
+    y = np.array(range(1,6))
+    shift = lambda u: u + 3
+    two_sample_shift(x, y, seed=5, shift=shift)
+        
 
 @attr('slow')
 def test_two_sample_conf_int():
@@ -132,7 +152,7 @@ def test_two_sample_conf_int():
     x = np.array(range(5))
     y = np.array(range(1,6))
     res = two_sample_conf_int(x, y, seed=prng)
-    expected_ci =  (-3.206390136239586, 1.0738676797240316)
+    expected_ci =  (-3.0442353, 1.0000037)
     np.testing.assert_almost_equal(res, expected_ci)
     res = two_sample_conf_int(x, y, seed=prng, alternative="upper")
     expected_ci = (-5, 1)
@@ -149,7 +169,7 @@ def test_two_sample_conf_int():
     # Specify shift with a multiplicative pair
     shift = (lambda u, d: u*d, lambda u,d: u/d)
     res = two_sample_conf_int(x, y, seed=5, shift=shift)
-    np.testing.assert_almost_equal(res, (-1, 1.75))
+    np.testing.assert_almost_equal(res, (-1, -1))
 
 
 @raises(AssertionError)
