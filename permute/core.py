72ffb0347c07ee9baba41d93b1a84b2f19c0b60a
Kellie Ottoboni <kellieotto@berkeley.edu>
2015-09-25 15:04:49 -0700

    RFT: pull out main permutation routine and put it into two_sample_core; create wrappers two_sample and two_sample_shift

--- permute/core.py
+++ permute/core.py
@@ -46,7 +46,165 @@ def corr(x, y, reps=10**4, seed=None):
     return tst, left_pv, right_pv, two_sided_pv, sims
 
 
+def two_sample_core(potential_outcomes_all, nx, tst_stat,
+                    reps=10**5, keep_dist=False, seed=None):
+    '''
+    Main workhorse function for two_sample and two_sample_shift
+
+    Parameters
+    ----------
+    potential_outcomes_all : array-like
+        2D array of potential outcomes under treatment (1st column) 
+        and control (2nd column). To be passed in from potential_outcomes
+    nx : int
+        Size of the treatment group x
+    reps : int
+        number of repetitions
+    tst_stat: function
+        The test statistic
+    keep_dist : bool
+        flag for whether to store and return the array of values
+        of the irr test statistic
+    seed : RandomState instance or {None, int, RandomState instance}
+        If None, the pseudorandom number generator is the RandomState
+        instance used by `np.random`;
+        If int, seed is the seed used by the random number generator;
+        If RandomState instance, seed is the pseudorandom number generator
+        
+    Returns
+    -------
+    float
+        the estimated p-value
+    float
+        the test statistic
+    list
+        The distribution of test statistics.
+        These values are only returned if `keep_dist` == True
+    '''
+    prng = get_prng(seed)
+    
+    rr = list(range(potential_outcomes_all.shape[0]))
+    tst = tst_stat(potential_outcomes_all[:nx,0], potential_outcomes_all[nx:,1])
+    
+    if keep_dist:
+        dist = np.empty(reps)
+        for i in range(reps):
+            prng.shuffle(rr)
+            pp = np.take(potential_outcomes_all, rr, axis=0)
+            dist[i] = tst_stat(pp[:nx, 0], pp[nx:, 1])
+        hits = np.sum(dist >= tst)
+        return hits/reps, dist
+    else:
+        hits = 0
+        for i in range(reps):
+            prng.shuffle(rr)
+            pp = np.take(potential_outcomes_all, rr, axis=0)
+            hits += tst_stat(pp[:nx, 0], pp[nx:, 1]) >= tst
+        return hits/reps
+
+
 def two_sample(x, y, reps=10**5, stat='mean', alternative="greater",
+               keep_dist=False, seed=None, shift=None):
+    """
+    One-sided or two-sided, two-sample permutation test for equality of
+    two means, with p-value estimated by simulated random sampling with
+    reps replications.
+
+    Tests the hypothesis that x and y are a random partition of x,y
+    against the alternative that x comes from a population with mean
+
+    (a) greater than that of the population from which y comes,
+        if side = 'greater'
+    (b) less than that of the population from which y comes,
+        if side = 'less'
+    (c) different from that of the population from which y comes,
+        if side = 'two-sided'
+
+    If ``keep_dist``, return the distribution of values of the test statistic;
+    otherwise, return only the number of permutations for which the value of
+    the test statistic and p-value.
+
+    Parameters
+    ----------
+    x : array-like
+        Sample 1
+    y : array-like
+        Sample 2
+    reps : int
+        number of repetitions
+    stat : {'mean', 't'}
+        The test statistic.
+
+        (a) If stat == 'mean', the test statistic is (mean(x) - mean(y))
+            (equivalently, sum(x), since those are monotonically related)
+        (b) If stat == 't', the test statistic is the two-sample t-statistic--
+            but the p-value is still estimated by the randomization,
+            approximating the permutation distribution.
+            The t-statistic is computed using scipy.stats.ttest_ind
+        (c) If stat is a function (a callable object), the test statistic is
+            that function.  The function should take a permutation of the pooled
+            data and compute the test function from it. For instance, if the
+            test statistic is the Kolmogorov-Smirnov distance between the
+            empirical distributions of the two samples, max_t |F_x(t) - F_y(t)|,
+            the test statistic could be written:
+
+            f = lambda u: np.max( \
+                [abs(sum(u[:len(x)]<=v)/len(x)-sum(u[len(x):]<=v)/len(y)) for v in u]\
+                )
+
+    alternative : {'greater', 'less', 'two-sided'}
+        The alternative hypothesis to test
+    keep_dist : bool
+        flag for whether to store and return the array of values
+        of the irr test statistic
+    seed : RandomState instance or {None, int, RandomState instance}
+        If None, the pseudorandom number generator is the RandomState
+        instance used by `np.random`;
+        If int, seed is the seed used by the random number generator;
+        If RandomState instance, seed is the pseudorandom number generator
+
+    Returns
+    -------
+    float
+        the estimated p-value
+    float
+        the test statistic
+    list
+        The distribution of test statistics.
+        These values are only returned if `keep_dist` == True
+    """    
+    # Set up potential outcomes; under the null, all units are exchangeable
+    pot_out_all = np.column_stack([np.concatenate([x, y]), np.concatenate([x, y])])
+    
+    # If stat is callable, use it as the test function. Otherwise, look in the dictionary
+    stats = {
+        'mean': lambda u,v: np.mean(u) - np.mean(v),
+        't': lambda u,v: ttest_ind(u, v, equal_var=True)[0]
+    }
+    if callable(stat):
+        tst_fun = stat
+    else:
+        tst_fun = stats[stat]
+        
+    theStat = {
+        'greater': tst_fun,
+        'less': lambda u,v: -tst_fun(u, v),
+        'two-sided': lambda u,v: math.fabs(tst_fun(u, v))
+    }
+
+    nx = len(x)    
+    observed_tst = tst_fun(pot_out_all[:nx,0], pot_out_all[nx:,1])
+    
+    res = two_sample_core(pot_out_all, nx, theStat[alternative],
+            reps=reps, keep_dist=keep_dist, seed=seed)
+    
+    if keep_dist:
+        return res[0], observed_tst, res[1]
+    else:
+        return res, observed_tst
+
+
+def two_sample_shift(x, y, reps=10**5, stat='mean', alternative="greater",
                keep_dist=False, seed=None, shift=None):
     """
     One-sided or two-sided, two-sample permutation test for equality of
@@ -108,12 +266,9 @@ def two_sample(x, y, reps=10**5, stat='mean', alternative="greater",
     shift : float
         The relationship between x and y under the null hypothesis.
         
-        (a) If None, the shift is assumed to be 0. This is the null hypothesis one 
-            typically tests.
-        (b) A constant scalar shift in the distribution of y. That is, x is equal 
+        (a) A constant scalar shift in the distribution of y. That is, x is equal 
             in distribution to y + shift.
-        (c) An invertible function, so under the null x_i = f(y_i) for all individuals i
-        (d) A tuple containing the function and its inverse (f, finverse), so
+        (b) A tuple containing the function and its inverse (f, finverse), so
             x_i = f(y_i) and y_i = finverse(x_i)
         
     Returns
@@ -125,15 +280,9 @@ def two_sample(x, y, reps=10**5, stat='mean', alternative="greater",
     list
         The distribution of test statistics.
         These values are only returned if `keep_dist` == True
-    """
-    prng = get_prng(seed)
-    
+    """    
     # Set up potential outcomes according to shift
-    if shift is None:
-        pot_outx = np.concatenate([x, y])
-        pot_outy = np.concatenate([x, y])
-        pot_out_all = np.column_stack([pot_outx, pot_outy])
-    elif isinstance(shift, float) or isinstance(shift, int):
+    if isinstance(shift, float) or isinstance(shift, int):
         pot_outx = np.concatenate([x, y + shift]) # Potential outcomes for all units under treatment
         pot_outy = np.concatenate([x - shift, y]) # Potential outcomes for all units under control
         pot_out_all = np.column_stack([pot_outx, pot_outy])
@@ -160,26 +309,16 @@ def two_sample(x, y, reps=10**5, stat='mean', alternative="greater",
         'two-sided': lambda u,v: math.fabs(tst_fun(u, v))
     }
 
-    rr = list(range(pot_out_all.shape[0]))
     nx = len(x)    
     observed_tst = tst_fun(pot_out_all[:nx,0], pot_out_all[nx:,1])
-    tst = theStat[alternative](pot_out_all[:nx,0], pot_out_all[nx:,1])
+    
+    res = two_sample_core(pot_out_all, nx, theStat[alternative],
+            reps=reps, keep_dist=keep_dist, seed=seed)
     
     if keep_dist:
-        dist = np.empty(reps)
-        for i in range(reps):
-            prng.shuffle(rr)
-            pp = np.take(pot_out_all, rr, axis=0)
-            dist[i] = theStat[alternative](pp[:nx, 0], pp[nx:, 1])
-        hits = np.sum(dist >= tst)
-        return hits/reps, observed_tst, dist
+        return res[0], observed_tst, res[1]
     else:
-        hits = 0
-        for i in range(reps):
-            prng.shuffle(rr)
-            pp = np.take(pot_out_all, rr, axis=0)
-            hits += theStat[alternative](pp[:nx, 0], pp[nx:, 1]) >= tst
-        return hits/reps, observed_tst
+        return res, observed_tst
 
 
 def two_sample_conf_int(x, y, cl=0.95, alternative="two-sided", seed=None,
