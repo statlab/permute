be5a066086aef444ec8250ac7016c81eb0594bc2
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-05-26 14:58:23 -0700

    ENH: Add stratified_two_sample, a function that conforms to the way we wrote two_sample and can do either the raw difference in means OR do within-strata diff in means and then sum them over strata.

--- permute/stratified.py
+++ permute/stratified.py
@@ -223,3 +223,125 @@ def stratified_permutationtest(group, condition, response, alternative='greater'
 
         right_pv = np.sum(dist >= tst) / reps
         return thePvalue[alternative](right_pv), tst, dist
+
+
+def stratified_two_sample(group, condition, response, stat='mean', alternative="greater", 
+                            reps=10**5,  keep_dist=False, seed=None):
+    """
+    One-sided or two-sided, two-sample permutation test for equality of
+    two means, with p-value estimated by simulated random sampling with
+    reps replications.
+
+    Tests the hypothesis that x and y are a random partition of x,y
+    against the alternative that x comes from a population with mean
+
+    (a) greater than that of the population from which y comes,
+        if side = 'greater'
+    (b) less than that of the population from which y comes,
+        if side = 'less'
+    (c) different from that of the population from which y comes,
+        if side = 'two-sided'
+
+    Permutations are carried out within the given groups.  Under the null hypothesis,
+    observations within each group are exchangeable.
+
+    If ``keep_dist``, return the distribution of values of the test statistic;
+    otherwise, return only the number of permutations for which the value of
+    the test statistic and p-value.
+
+    Parameters
+    ----------
+    group : array-like
+        Group memberships
+    condition : array-like
+        Treatment conditions, of the same length as group
+    response : array-like
+        Responses, of the same length as group
+    stat : {'mean', 't'}
+        The test statistic.
+
+        (a) If stat == 'mean', the test statistic is (mean(x) - mean(y))
+            (equivalently, sum(x), since those are monotonically related), omitting
+            NaNs, which therefore can be used to code non-responders
+        (b) If stat == 't', the test statistic is the two-sample t-statistic--
+            but the p-value is still estimated by the randomization,
+            approximating the permutation distribution.
+            The t-statistic is computed using scipy.stats.ttest_ind
+        (c) If stat == 'mean_within_strata', the test statistic is the difference
+            in means within each stratum, added across strata.
+        (d) If stat is a function (a callable object), the test statistic is
+            that function.  The function should take a permutation of the pooled
+            data and compute the test function from it. For instance, if the
+            test statistic is the Kolmogorov-Smirnov distance between the
+            empirical distributions of the two samples, max_t |F_x(t) - F_y(t)|,
+            the test statistic could be written:
+
+            f = lambda u: np.max( \
+                [abs(sum(u[:len(x)]<=v)/len(x)-sum(u[len(x):]<=v)/len(y)) for v in u]\
+                )        
+    alternative : {'greater', 'less', 'two-sided'}
+        The alternative hypothesis to test
+    reps : int
+        Number of permutations
+    keep_dist : bool
+        flag for whether to store and return the array of values
+        of the test statistic
+    seed : RandomState instance or {None, int, RandomState instance}
+        If None, the pseudorandom number generator is the RandomState
+        instance used by `np.random`;
+        If int, seed is the seed used by the random number generator;
+        If RandomState instance, seed is the pseudorandom number generator.
+
+    Returns
+    -------
+    float
+        the estimated p-value
+    float
+        the test statistic
+    list
+        The distribution of test statistics.
+        These values are only returned if `keep_dist` == True        
+    """
+    
+    prng = get_prng(seed)
+    
+    ordering = condition.argsort()
+    response = response[ordering]
+    condition = condition[ordering]
+    group = group[ordering]
+    ntreat = np.bincount(condition)[0]
+
+    groups = np.unique(group)
+    conditions = np.unique(condition)
+    # If stat is callable, use it as the test function. Otherwise, look in the dictionary
+    stats = {
+        'mean': lambda u: np.nanmean(u[:ntreat]) - np.nanmean(u[ntreat:]),
+        't': lambda u: ttest_ind(
+            u[:len(x)][~np.isnan(u[:ntreat])], 
+            u[len(x):][~np.isnan(u[ntreat:])], 
+            equal_var=True)[0],
+        'mean_within_strata': lambda u: stratified_permutationtest_mean(group, condition,
+                                    u, groups, conditions)
+    }
+    if callable(stat):
+        tst_fun = stat
+    else:
+        tst_fun = stats[stat]
+
+    thePvalue = {
+        'greater': lambda p: p,
+        'less': lambda p: 1 - p,
+        'two-sided': lambda p: 2 * np.min([p, 1 - p])
+    }
+    observed_tst = tst_fun(response)
+    
+    if keep_dist:
+        dist = np.empty(reps)
+        for i in range(reps):
+            dist[i] = tst_fun(permute_within_groups(response, group, seed=prng))
+        hits = np.sum(dist >= observed_tst)
+        return thePvalue[alternative](hits/reps), observed_tst, dist
+    else:
+        hits = np.sum([(tst_fun(permute_within_groups(response, group, seed=prng)) >= observed_tst)
+                       for i in range(reps)])
+        return thePvalue[alternative](hits/reps), observed_tst
