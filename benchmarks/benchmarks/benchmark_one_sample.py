3e657059f32df386f22b5568e44cb72416627ef1
Jarrod Millman <jarrod.millman@gmail.com>
2016-05-30 16:02:51 -0700

    RFT: bundle into a class, add README.md (modified from numpy)

--- benchmarks/benchmarks/benchmark_one_sample.py
+++ benchmarks/benchmarks/benchmark_one_sample.py
@@ -1,23 +1,26 @@
-from numpy import array, mean, sqrt
-from numpy.random import RandomState
+import numpy as np
+
 from permute.core import one_sample
 
 
-def time_one_sample():
-    t = [689, 656, 668, 660, 679, 663, 664, 647, 694, 633, 653]
-    c = [657, 623, 652, 654, 658, 646, 600, 640, 605, 635, 642]
-    d = array(t) - array(c)
-    n = len(d)
+class OneSample:
 
-    prng = RandomState(42)
-    p, diff_means, dist = one_sample(d, stat='t', keep_dist=True, seed=prng)
+    def setup(self):
+        self.prng = np.random.RandomState(42)
+        # short example
+        self.t = [689, 656, 668, 660, 679, 663, 664, 647, 694, 633, 653]
+        self.c = [657, 623, 652, 654, 658, 646, 600, 640, 605, 635, 642]
+        self.d = np.array(self.t) - np.array(self.c)
+        # longer example
+        self.t_long = self.t * 10
+        self.c_long = self.c * 10
+        self.d_long = np.array(self.t_long) - np.array(self.c_long)
 
 
-def time_one_sample_large():
-    t = [689, 656, 668, 660, 679, 663, 664, 647, 694, 633, 653] * 10
-    c = [657, 623, 652, 654, 658, 646, 600, 640, 605, 635, 642] * 10
-    d = array(t) - array(c)
-    n = len(d)
+    def time_one_sample(self):
+        p, diffs, dist = one_sample(self.d, stat='t', keep_dist=True, seed=self.prng)
 
-    prng = RandomState(42)
-    p, diff_means, dist = one_sample(d, stat='t', keep_dist=True, seed=prng)
+
+    def time_one_sample_large(self):
+        p, diffs, dist = one_sample(self.d, stat='t', keep_dist=True, seed=self.prng)
+
