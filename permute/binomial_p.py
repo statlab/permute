0c5bde6d989e40a18ae3a94e1e64080dc48d79e0
Lauren Hong <lauren.hong@berkeley.edu>
2017-06-01 03:45:15 -0700

    Add files via upload

--- permute/binomial_p.py
+++ permute/binomial_p.py
@@ -1,14 +1,92 @@
 """
-Binomial Probability Test
+Binomial Permutation Test
 
 """
 import scipy
+import numpy as np
 from scipy.special import comb
 
-def binomial_test(n, p, y, t, a, Ho_sign):
+
+## User's Function ##
+
+def binomial_p(population, successes, n, y, Ho_sign):
+	"""
+	Performs binomoial permutation test.
+
+	Parameters
+	----------
+	population : list
+	   list of all elements in population represented by {0, 1}
+	successes : int
+	   number of successes in population
+	y : int
+	   hypothesized number of successes in n trials
+	n : int
+	   number of trials
+	Ho_sign : string of equality sign in null hypothesis
+	   random variable Z is "==", "<=", "!=", ">", "<", or ">=" y
+
+	
+	Returns
+	-------
+	float
+	   p-value binomial probability test
+	float
+	   p-value of permutation test 
 
 	"""
-	Returns the p-value and conclusion of the binomial probability test.
+	p = successes /len(population)
+	original_statistic = binomial_test(n, p, y, Ho_sign)
+
+	length = len(population)
+	permutations = int(comb(length, n))
+
+	values = []
+	def permute():
+		nonlocal values
+		new_sample = []
+		for i in range(n):
+			x = np.random.choice(population)
+			new_sample.append(x)
+		values.append(len([x for x in new_sample if x == 0])) 
+
+
+	for p in range(permutations):
+		permute()
+
+
+	count = 0
+	for val in values:
+		if Ho_sign == "==":
+			if val == y:
+				count += 1
+		elif Ho_sign == "<=":
+			if val <= y:
+				count += 1
+		elif Ho_sign == "!=":
+			if val != y:
+				count += 1
+		elif Ho_sign == ">":
+			if val > y:
+				count += 1
+		elif Ho_sign == "<":
+			if val < y:
+				count += 1
+		elif Ho_sign == ">=":
+			if val >= y:
+				count += 1
+	permute_p = count / permutations
+	return (original_statistic, permute_p)
+
+
+
+
+## Private Helper Function ##
+
+def binomial_test(n, p, y, Ho_sign, t=2):
+
+	"""
+	Returns the p-value of the binomial probability test.
 
 	Parameters
 	----------
@@ -18,20 +96,16 @@ def binomial_test(n, p, y, t, a, Ho_sign):
 	   success probability
 	y : int
 	   hypothesized number of successes in n trials
-	t : int
+	t : int (default 2)
 	   for one-tailed test, input 1
 	   for two-tailed test, input 2
-	a : float
-	   significance level
 	Ho_sign : string of equality sign in null hypothesis
 	   random variable Z is "==", "<=", "!=", ">", "<", or ">=" y
 
 	Returns
 	-------
-	string
+	float
 	   p-value of test
-	string
-	   conclusion of test
 
 	"""
 
@@ -45,10 +119,6 @@ def binomial_test(n, p, y, t, a, Ho_sign):
 	}
 
 	if n == 1:
-		if p <= a:
-			print("Reject the null hypothesis.")
-		else:
-			print("Fail to reject the null hypothesis.")
 		return p 
 
 	alt_sign = complements[Ho_sign]
@@ -75,12 +145,9 @@ def binomial_test(n, p, y, t, a, Ho_sign):
 
 	if t == 2:
 		prob = 2 * prob
+	return prob
 
-	if prob <= a:
-		print("Reject the null hypothesis.")
-	else:
-		print("Fail to reject the null hypothesis.")
-	return "p-value: {}".format(prob)
+	
 
 
 
