3c7331f4c50700d877e1f68fcb8a565cde6b4190
Kellie Ottoboni <kellieotto@berkeley.edu>
2017-10-23 14:42:14 -0700

    TST: fix binomial_p test error and add check for inputs ValueError

--- permute/tests/test_binomialp.py
+++ permute/tests/test_binomialp.py
@@ -13,8 +13,12 @@ def test_binomial_p():
     assert_almost_equal(binomial_p(5, 10, 0.5, 10**5, 'greater')[0], 1-binom.cdf(4, 10, 0.5), 2)
     assert_almost_equal(binomial_p(5, 10, 0.5, 10**5, 'less')[0], binom.cdf(5, 10, 0.5), 2)
     assert_almost_equal(binomial_p(5, 10, 0.5, 10**5, 'two-sided')[0], 1, 2)
-    assert_equal(len(binomial_p(5, 10, 0.5, 10, 'greater', keep_dist=True)), 2)
+    assert_equal(len(binomial_p(5, 10, 0.5, 10, 'greater', keep_dist=True)), 3)
     
     res1 = binomial_p(5, 10, 0.5, 10**5, 'greater', keep_dist=True, seed=12345)
     res2 = binomial_p(5, 10, 0.5, 10**5, 'greater', keep_dist=False, seed=12345)
     assert_equal(res1[0], res2[0])
+    
+@raises(ValueError)
+def test_binomial_badinput():
+    binomial_p(10, 5, 0.5)
