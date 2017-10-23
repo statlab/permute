f07331b8bfe3c43e694501b44ffadfae2dc262c6
Kellie Ottoboni <kellieotto@berkeley.edu>
2017-10-23 13:22:44 -0700

    BUG: fix null hypothesis bug in hypergeom

--- permute/hypergeom.py
+++ permute/hypergeom.py
@@ -1,105 +1,78 @@
 """
 Hypergeometric Test
 """
-import scipy
 import numpy as np
-from scipy.special import comb
 from .utils import get_prng
 
-def hypergeom(population, n, g, reps=10**5, alternative='greater', keep_dist=False, seed=None):
-	
-	"""
-	Parameters
-	----------
-	population : array-like
-	   list of elements consisting of x in {0, 1} where 0 represents a failure and 1 represents a success
-	n : int
-	   sample size
-	g : int
-	   hypothesized number of good elements in sample
-	reps : int
-	   number of repetitions (default: 10**5)
-	alternative : {'greater', 'less', 'two-sided'}
-	   alternative hypothesis to test (default: 'greater')
-	keep_dist : boolean
-	   flag for whether to store and return the array of values of the test statistics (default: false)
-	seed : RandomState instance or {None, int, RandomState instance}
+def hypergeometric(x, N, n, G, reps=10**5, alternative='greater', keep_dist=False, seed=None):
+    
+    """
+    Parameters
+    ----------
+    x : int
+        number of `good` elements observed in the sample
+    N : int
+        population size
+    n : int
+       sample size
+    G : int
+       hypothesized number of good elements in population
+    reps : int
+       number of repetitions (default: 10**5)
+    alternative : {'greater', 'less', 'two-sided'}
+       alternative hypothesis to test (default: 'greater')
+    keep_dist : boolean
+       flag for whether to store and return the array of values of the test statistics (default: false)
+    seed : RandomState instance or {None, int, RandomState instance}
         If None, the pseudorandom number generator is the RandomState
         instance used by `np.random`;
         If int, seed is the seed used by the random number generator;
         If RandomState instance, seed is the pseudorandom number generator
-	Returns
-	-------
-	float
-	   estimated p-value
-	float 
-	   test statistic
-	list
-	   distribution of test statistics (only if keep_dist == True)
-	"""
+    Returns
+    -------
+    float
+       estimated p-value
+    float 
+       test statistic
+    list
+       distribution of test statistics (only if keep_dist == True)
+    """
 
+    prng = get_prng(seed)
 
-	original_ts = sum(population) / len(population)
+    def generate():
+        return prng.hypergeometric(G, N-G, n)
 
-	prng = get_prng(seed)
+    if keep_dist:
+        permutations = np.empty(reps)
+        for i in range(reps):
+            permutations[i] = generate()
+        if alternative == 'two-sided':
+            hits_up = np.sum(permutations >= x)
+            hits_low = np.sum(permutations <= x)
+            p_value = 2*np.min(hits_up/reps, hits_low/reps, 0.5)
+        elif alternative == 'greater':
+            p_value = np.mean(permutations >= x)
+        else:
+            p_value = np.mean(permutations <= x)
+        return p_value, x, permutations
 
-	pop_G = sum(population)
-	pop_B = len(population) - pop_G
+    else:
+        hits_up = 0
+        hits_low = 0
+        for i in range(reps):
+            ts = generate()
+            hits_up += (ts >= x)
+            hits_low += (ts <= x)
+        
+        if alternative == 'two-sided':
+            p_value = 2*np.min([hits_up/reps, hits_low/reps, 0.5])
+        elif alternative == 'greater':
+            p_value = hits_up/reps
+        else:
+            p_value = hits_low/reps
 
-	permutations = []
-
-	def generate():
-
-		return prng.hypergeometric(pop_G, pop_B, n)
-
-	while reps >= 0:
-		ts = generate()
-		permutations.append(ts)
-		reps -= 1
-
-	simulations = list(permutations)
-
-	permutations2 = list(permutations)
-
-
-
-	alternative_func = {
-	'greater': lambda thing: thing > g,
-	'less': lambda thing: thing < g
-	}
-
-	
-	if alternative == "two-sided":
-		count = 0
-		while len(permutations) > 0:
-			val = permutations.pop()
-			if alternative_func['greater'](val):
-				count += 1
-		p_valueG = count /len(simulations)
-		counter = 0
-		while len(permutations2) > 0:
-			val = permutations2.pop()
-			if alternative_func['less'](val):
-				counter += 1
-		p_valueL = counter / len(simulations)
-		p_value = 2 * min(p_valueG, p_valueL)
-
-
-	else:
-		count = 0
-		while len(permutations) > 0:
-			val = permutations.pop()
-			if alternative_func[alternative](val):
-				count += 1
-		p_value = count / len(simulations)
-
-	
-
-
-	if keep_dist == True:
-		return p_value, original_ts, simulations
-
-	return p_value, original_ts
+        return p_value, x
 
 
 
