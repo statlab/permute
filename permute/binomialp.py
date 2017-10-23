54510e84f74c2881e264c33e01442b527cd1c68f
Kellie Ottoboni <kellieotto@berkeley.edu>
2017-10-23 15:12:19 -0700

    BUG: add from future import division to binomial_p and hypergeometric

--- permute/binomialp.py
+++ permute/binomialp.py
@@ -1,9 +1,10 @@
 """
 Binomial Permutation Test
 """
-import scipy
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
 import numpy as np
-from scipy.special import comb
 from .utils import get_prng
 
 
