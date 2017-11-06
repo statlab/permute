5e0b2b22f05a22a7712d298b2e0c398c5570a226
Andrew Linxie <alinxie@berkeley.edu>
2017-11-05 16:28:15 -0800

    ENH: One sample confidence test and intervals for shift model. No comments/tests yet.

--- permute/core.py
+++ permute/core.py
@@ -520,7 +520,8 @@ def one_sample(x, y=None, reps=10**5, stat='mean', alternative="greater",
     }
     stats = {
         'mean': lambda u: np.mean(u),
-        't': lambda u: ttest_1samp(u, 0)[0]
+        't': lambda u: ttest_1samp(u, 0)[0],
+        'median': lambda u: np.median(u)
     }
     if callable(stat):
         tst_fun = stat
@@ -528,6 +529,7 @@ def one_sample(x, y=None, reps=10**5, stat='mean', alternative="greater",
         tst_fun = stats[stat]
 
     tst = tst_fun(z)
+
     n = len(z)
     if keep_dist:
         dist = []
@@ -539,3 +541,237 @@ def one_sample(x, y=None, reps=10**5, stat='mean', alternative="greater",
         hits = np.sum([(tst_fun(z * (1 - 2 * prng.binomial(1, .5, size=n)))) >= tst
                        for i in range(reps)])
         return thePvalue[alternative](hits / reps), tst
+
+
+def one_sample_shift(x, y=None, reps=10**5, stat='mean', alternative="greater",
+               keep_dist=False, seed=None, shift = None):
+    r"""
+    One-sided or two-sided, one-sample permutation test for the mean,
+    with p-value estimated by simulated random sampling with
+    reps replications.
+
+    Alternatively, a permutation test for equality of means of two paired
+    samples.
+
+    This function assumed a shift model.
+
+    Tests the hypothesis that x is distributed symmetrically symmetric about 0
+    (or x and y have the same center) against the alternative that x comes from
+    a population with mean
+
+    (a) greater than 0 (greater than that of the population from which y comes),
+        if side = 'greater'
+    (b) less than 0 (less than that of the population from which y comes),
+        if side = 'less'
+    (c) different from 0 (different from that of the population from which y comes),
+        if side = 'two-sided'
+
+    If ``keep_dist``, return the distribution of values of the test statistic;
+    otherwise, return only the number of permutations for which the value of
+    the test statistic and p-value.
+
+    Parameters
+    ----------
+    x : array-like
+        Sample 1
+    y : array-like
+        Sample 2. Must preserve the order of pairs with x.
+        If None, x is taken to be the one sample.
+    reps : int
+        number of repetitions
+    stat : {'mean', 't'}
+        The test statistic. The statistic is computed based on either z = x or
+        z = x - y, if y is specified.
+
+        (a) If stat == 'mean', the test statistic is mean(z).
+        (b) If stat == 't', the test statistic is the t-statistic--
+            but the p-value is still estimated by the randomization,
+            approximating the permutation distribution.
+        (c) If stat is a function (a callable object), the test statistic is
+            that function.  The function should take a permutation of the
+            data and compute the test function from it. For instance, if the
+            test statistic is the maximum absolute value, $\max_i |z_i|$,
+            the test statistic could be written:
+
+            f = lambda u: np.max(abs(u))
+    alternative : {'greater', 'less', 'two-sided'}
+        The alternative hypothesis to test
+    keep_dist : bool
+        flag for whether to store and return the array of values
+        of the irr test statistic
+    seed : RandomState instance or {None, int, RandomState instance}
+        If None, the pseudorandom number generator is the RandomState
+        instance used by `np.random`;
+        If int, seed is the seed used by the random number generator;
+        If RandomState instance, seed is the pseudorandom number generator
+    shift : float
+        Assumption of symmetry around the shift, d.
+
+    Returns
+    -------
+    float
+        the estimated p-value
+    float
+        the test statistic
+    list
+        The distribution of test statistics.
+        These values are only returned if `keep_dist` == True
+    """
+    prng = get_prng(seed)
+
+    if y is None:
+        z = x
+    elif len(x) != len(y):
+        raise ValueError('x and y must be pairs')
+    else:
+        z = np.array(x) - np.array(y)
+
+    thePvalue = {
+        'greater': lambda p: p,
+        'less': lambda p: 1 - p,
+        'two-sided': lambda p: 2 * np.min([p, 1 - p])
+    }
+    stats = {
+        'mean': lambda u: np.mean(u),
+        't': lambda u: ttest_1samp(u, 0)[0],
+        'median': lambda u: np.median(u)
+    }
+    if callable(stat):
+        tst_fun = stat
+    else:
+        tst_fun = stats[stat]
+
+    tst = tst_fun(z)
+
+    if shift is None:
+        shift = float(0)
+
+    n = len(z)
+    if keep_dist:
+        dist = []
+        for i in range(reps):
+            dist.append(tst_fun(shift + (z - shift) * (1 - 2 * prng.binomial(1, .5, size=n))))
+        hits = np.sum(dist >= tst)
+        return thePvalue[alternative](hits / reps), tst, dist
+    else:
+        hits = np.sum([(tst_fun(shift + (z - shift) * (1 - 2 * prng.binomial(1, .5, size=n)))) >= tst
+                       for i in range(reps)])
+        return thePvalue[alternative](hits / reps), tst
+
+
+
+
+def one_sample_conf_int(x, y = None, cl=0.95, alternative="two-sided", seed=None,
+                        reps=10**4, stat="mean"):
+    """
+    One-sided or two-sided confidence interval for the parameter determining
+    the treatment effect.  The default is the "shift model", where we are
+    interested in the parameter d such that x is equal in distribution to
+    y + d. In general, if we have some family of invertible functions parameterized
+    by d, we'd like to find d such that x is equal in distribution to f(y, d).
+
+    Parameters
+    ----------
+    x : array-like
+        Sample 1
+    y : array-like
+        Sample 2
+    cl : float in (0, 1)
+        The desired confidence level. Default 0.95.
+    alternative : {"two-sided", "lower", "upper"}
+        Indicates the alternative hypothesis.
+    seed : RandomState instance or {None, int, RandomState instance}
+        If None, the pseudorandom number generator is the RandomState
+        instance used by `np.random`;
+        If int, seed is the seed used by the random number generator;
+        If RandomState instance, seed is the pseudorandom number generator
+    reps : int
+        number of repetitions in two_sample
+    stat : {'mean', 't'}
+        The test statistic.
+
+        (a) If stat == 'mean', the test statistic is (mean(x) - mean(y))
+            (equivalently, sum(x), since those are monotonically related)
+        (b) If stat == 't', the test statistic is the two-sample t-statistic--
+            but the p-value is still estimated by the randomization,
+            approximating the permutation distribution.
+            The t-statistic is computed using scipy.stats.ttest_ind
+        (c) If stat is a function (a callable object), the test statistic is
+            that function.  The function should take a permutation of the pooled
+            data and compute the test function from it. For instance, if the
+            test statistic is the Kolmogorov-Smirnov distance between the
+            empirical distributions of the two samples, $\max_t |F_x(t) - F_y(t)|$,
+            the test statistic could be written:
+
+            f = lambda u: np.max( \
+                [abs(sum(u[:len(x)]<=v)/len(x)-sum(u[len(x):]<=v)/len(y)) for v in u]\
+                )
+    shift : float
+        The relationship between x and y under the null hypothesis.
+
+        (a) If None, the relationship is assumed to be additive (e.g. x = y+d)
+        (b) A tuple containing the function and its inverse $(f, f^{-1})$, so
+            $x_i = f(y_i, d)$ and $y_i = f^{-1}(x_i, d)$
+
+    Returns
+    -------
+    tuple
+        the estimated confidence limits
+
+    Notes
+    -----
+    xtol : float
+        Tolerance in brentq
+    rtol : float
+        Tolerance in brentq
+    maxiter : int
+        Maximum number of iterations in brentq
+    """
+    assert alternative in ("two-sided", "lower", "upper")
+
+    if shift is None:
+        shift_limit = max(abs(max(x) - min(y)), abs(max(y) - min(x)))
+        # FIXME: unused observed
+        # observed = np.mean(x) - np.mean(y)
+    elif isinstance(shift, tuple):
+        assert (callable(shift[0])), "Supply f and finverse in shift tuple"
+        assert (callable(shift[1])), "Supply f and finverse in shift tuple"
+        f = shift[0]
+        finverse = shift[1]
+        # Check that f is increasing in d; this is very ad hoc!
+        assert (f(5, 1) < f(5, 2)), "f must be increasing in the parameter d"
+        shift_limit = max(abs(fsolve(lambda d: f(max(y), d) - min(x), 0)),
+                          abs(fsolve(lambda d: f(min(y), d) - max(x), 0)))
+        # FIXME: unused observed
+        # observed = fsolve(lambda d: np.mean(x) - np.mean(f(y, d)), 0)
+    else:
+        raise ValueError("Bad input for shift")
+    ci_low = -shift_limit
+    ci_upp = shift_limit
+    max_around_zero = np.max([np.max(x), np.max(-x)])
+    ci_low = -max_around_zero
+    ci_upp = max_around_zero
+
+    if alternative == 'two-sided':
+        cl = 1 - (1 - cl) / 2
+
+    if alternative != "upper":
+        # if shift is None:
+        #     g = lambda q: cl - one_sample_shift(x, y, alternative="less", seed=seed, reps=reps, stat=stat, shift=q)[0]
+        # else:
+        g = lambda q: cl - one_sample_shift(x, y, alternative="less", seed=seed, \
+            reps=reps, stat=stat, shift=q)[0]
+        ci_low = brentq(g, -2 * max_around_zero, 2 * max_around_zero)
+
+    if alternative != "lower":
+        # if shift is None:
+        #     g = lambda q: cl - one_sample_shift(x, y, alternative="greater", seed=seed, reps=reps, stat=stat, shift=q)[0]
+        # else:
+        g = lambda q: cl - one_sample_shift(x, y, alternative="greater", seed=seed, \
+            reps=reps, stat=stat, shift=q)[0]
+        ci_upp = brentq(g, -2 * max_around_zero, 2 * max_around_zero)
+
+    return ci_low, ci_upp
+
+
+    
