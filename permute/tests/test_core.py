295ec8e4472bd9ffbd63b20e36a42fa18e196613
Philip B. Stark <stark@risklaw.org>
2020-02-04 09:55:19 -0800

    TST: added logging to test_core.py

--- permute/tests/test_core.py
+++ permute/tests/test_core.py
@@ -28,6 +28,7 @@ def test_corr():
     assert_equal(res1[0], 1)
     assert_equal(res2[0], 1)
     assert_almost_equal(res1[1], res2[1], decimal=1)
+    print("finished test 1 in test_corr()")
 
     y = prng.randint(0, 5, size=10)
     res1 = corr(x, y, alternative="less", seed=prng)
@@ -36,6 +37,7 @@ def test_corr():
     assert_equal(len(res2), 3)
     assert_equal(res1[0], res2[0])
     assert_almost_equal(res1[1], res2[1], decimal=1)
+    print("finished test 2 in test_corr()")
 
     res1 = corr(x, y, alternative="two-sided", seed=prng)
     res2 = corr(x, y, alternative="greater")
@@ -43,6 +45,7 @@ def test_corr():
     assert_equal(len(res2), 3)
     assert_equal(res1[0], res2[0])
     assert_almost_equal(res1[1], res2[1]*2, decimal=1)
+    print("finished test 3 in test_corr()")
 
 
 def test_spearman_corr():
@@ -51,12 +54,14 @@ def test_spearman_corr():
     y = np.array([1, 3, 5, 6, 9])
     xorder = np.array([1, 2, 3, 4, 5])
     res1 = corr(xorder, xorder, seed=prng)
+    print("finished test 1 in test_spearman_corr()")
     
     prng = SHA256(42)
     res2 = spearman_corr(x, y, seed=prng)
     assert_equal(res1[0], res2[0])
     assert_equal(res1[1], res2[1])
     assert_array_equal(res1[2], res2[2])
+    print("finished test 2 in test_spearman_corr()")
 
 
 @attr('slow')
@@ -69,8 +74,11 @@ def test_two_sample():
     res = two_sample(x, y, seed=42)
     expected = (1.0, -2.90532344604777)
     assert_almost_equal(res, expected, 5)
+    print("finished test 1 in test_two_sample()")
+
     res = two_sample(x, y, seed=42, plus1=False)
     assert_almost_equal(res, expected)
+    print("finished test 2 in test_two_sample()")
 
     # This one has keep_dist = True
     y = prng.normal(1.4, size=20)
@@ -81,6 +89,7 @@ def test_two_sample():
     assert_equal(res[1], expected[1])
     assert_almost_equal(res2[0], expected[0], 2)
     assert_equal(res2[1], expected[1])
+    print("finished test 3 in test_two_sample()")
 
     # Normal-normal, same means
     y = prng.normal(1, size=20)
@@ -88,6 +97,7 @@ def test_two_sample():
     expected = (0.66505000000000003, -0.13990200413154097)
     assert_almost_equal(res[0], expected[0], 2)
     assert_equal(res[1], expected[1])
+    print("finished test 4 in test_two_sample()")
 
     # Check the permutation distribution
     res = two_sample(x, y, seed=42, keep_dist=True)
@@ -98,6 +108,7 @@ def test_two_sample():
     assert_equal(res[1], expected_ts)
     assert_equal(len(res[2]), 100000)
     assert_almost_equal(res[2][:5], exp_dist_firstfive)
+    print("finished test 5 in test_two_sample()")
 
     # Define a lambda function (K-S test)
     f = lambda u, v: np.max(
@@ -107,6 +118,7 @@ def test_two_sample():
     expected = (0.62, 0.20000000000000007)
     assert_equal(res[0], expected[0])
     assert_equal(res[1], expected[1])
+    print("finished test 6 in test_two_sample()")
     
     # check tail computations
     x = np.ones(10)
@@ -115,14 +127,19 @@ def test_two_sample():
                keep_dist=False, seed=None, plus1=True)
     assert_equal(res[0], 1)
     assert_equal(res[1], 0)
+    print("finished test 7 in test_two_sample()")
+    
     res = two_sample(x, y, reps=10**2, stat='mean', alternative="less",
                keep_dist=False, seed=None, plus1=True)
     assert_equal(res[0], 1)
     assert_equal(res[1], 0)
+    print("finished test 8 in test_two_sample()")
+
     res = two_sample(x, y, reps=10**2, stat='mean', alternative="two-sided",
                keep_dist=False, seed=None, plus1=True)
     assert_equal(res[0], 1)
     assert_equal(res[1], 0)
+    print("finished test 9 in test_two_sample()")
     
 
 
@@ -143,28 +160,37 @@ def test_two_sample_shift():
     res = two_sample_shift(x, y, seed=42, shift=2, plus1=False)
     assert_equal(res[0], 1)
     assert_equal(res[1], expected_ts)
+    print("finished test 1 in test_two_sample_shift()")
+
     res2 = two_sample_shift(x, y, seed=42, shift=2, keep_dist=True)
     assert_almost_equal(res2[0], 1, 4)
     assert_equal(res2[1], expected_ts)
     assert_almost_equal(res2[2][:3], np.array(
         [1.140174 , 2.1491466, 2.6169429]))
+    print("finished test 2 in test_two_sample_shift()")
+
     res = two_sample_shift(x, y, seed=42, shift=2, alternative="less")
     assert_almost_equal(res[0], 0, 3)
     assert_equal(res[1], expected_ts)
+    print("finished test 3 in test_two_sample_shift()")
 
     # Test null with shift -3
     res = two_sample_shift(x, y, seed=42, shift=(f, finv))
     assert_almost_equal(res[0], 0.377, 2)
     assert_equal(res[1], expected_ts)
+    print("finished test 4 in test_two_sample_shift()")
+    
     res = two_sample_shift(x, y, seed=42, shift=(f, finv), alternative="less")
     assert_almost_equal(res[0], 0.622, 2)
     assert_equal(res[1], expected_ts)
+    print("finished test 5 in test_two_sample_shift()")
 
     # Test null with multiplicative shift
     res = two_sample_shift(x, y, seed=42,
         shift=(f_err, f_err_inv), alternative="two-sided")
     assert_almost_equal(res[0], 0, 3)
     assert_equal(res[1], expected_ts)
+    print("finished test 6 in test_two_sample_shift()")
 
     # Define a lambda function
     f = lambda u, v: np.max(u) - np.max(v)
@@ -172,6 +198,7 @@ def test_two_sample_shift():
     expected = (1, -3.2730653690015465)
     assert_equal(res[0], expected[0])
     assert_equal(res[1], expected[1])
+    print("finished test 7 in test_two_sample_shift()")
 
 
 @raises(ValueError)
@@ -181,6 +208,7 @@ def test_two_sample_bad_shift():
     y = np.array(range(1, 6))
     shift = lambda u: u + 3
     two_sample_shift(x, y, seed=5, shift=shift)
+    print("finished test 1 in test_two_sample_bad_shift()")
 
 
 @attr('slow')
@@ -193,22 +221,29 @@ def test_two_sample_conf_int():
     res = two_sample_conf_int(x, y, seed=prng)
     expected_ci = (-3.5, 1.0012461)
     assert_almost_equal(res, expected_ci)
+    print("finished test 1 in test_two_sample_conf_int()")
+
     res = two_sample_conf_int(x, y, seed=prng, alternative="upper")
     expected_ci = (-5, 1)
     assert_almost_equal(res, expected_ci)
+    print("finished test 2 in test_two_sample_conf_int()")
+    
     res = two_sample_conf_int(x, y, seed=prng, alternative="lower")
     expected_ci = (-3, 5)
     assert_almost_equal(res, expected_ci)
+    print("finished test 3 in test_two_sample_conf_int()")
 
     # Specify shift with a function pair
     shift = (lambda u, d: u + d, lambda u, d: u - d)
     res = two_sample_conf_int(x, y, seed=5, shift=shift)
     assert_almost_equal(res, (-3.5, 1))
+    print("finished test 4 in test_two_sample_conf_int()")
 
     # Specify shift with a multiplicative pair
     shift = (lambda u, d: u * d, lambda u, d: u / d)
     res = two_sample_conf_int(x, y, seed=5, shift=shift)
     assert_almost_equal(res, (-1, -1))
+    print("finished test 5 in test_two_sample_conf_int()")
 
 
 @raises(AssertionError)
@@ -218,6 +253,7 @@ def test_two_sample_conf_int_bad_shift():
     y = np.array(range(1, 6))
     shift = (lambda u, d: -d * u, lambda u, d: -u / d)
     two_sample_conf_int(x, y, seed=5, shift=shift)
+    print("finished test 1 in test_two_sample_conf_int_bad_shift()")
 
 
 def test_one_sample():
@@ -230,18 +266,23 @@ def test_one_sample():
     res = one_sample(x, seed=42, reps=100, plus1=False)
     assert_almost_equal(res[0], 0.05999999)
     assert_equal(res[1], 2)
+    print("finished test 1 in test_one_sample()")
+
     res = one_sample(x, seed=42, reps=100, plus1=True)
     assert_almost_equal(res[0], 0.069306930)
     assert_equal(res[1], 2)
+    print("finished test 2 in test_one_sample()")
 
     # case 2: paired sample
     res = one_sample(x, y, seed=42, reps=100, plus1=False)
     assert_equal(res[0], 0.05)
     assert_equal(res[1], 1)
+    print("finished test 3 in test_one_sample()")
 
     # case 3: break it - supply x and y, but not paired
     y = np.append(y, 10)
     assert_raises(ValueError, one_sample, x, y)
+    print("finished test 4 in test_one_sample()")
 
     # case 4: say keep_dist=True
     res = one_sample(x, seed=42, reps=100, keep_dist=True, plus1=False)
@@ -250,9 +291,11 @@ def test_one_sample():
     assert_equal(min(res[2]), -2)
     assert_equal(max(res[2]), 2)
     assert_equal(np.median(res[2]), 0)
+    print("finished test 5 in test_one_sample()")
 
     # case 5: use t as test statistic
     y = x + prng.normal(size=5)
     res = one_sample(x, y, seed=42, reps=100, stat="t", alternative="less", plus1=False)
     assert_almost_equal(res[0], 0.08)
     assert_almost_equal(res[1], -1.4491883)
+    print("finished test 6 in test_one_sample()")
